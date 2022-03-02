13f56a9d6de2716e8e33094a2b23af7b5350c820
jasminexo <56755376+jasminexo@users.noreply.github.com>
2022-03-02 21:51:26 +1100

    test comment

--- app/src/main/java/com/example/eloitteproject/MainActivity.java
+++ app/src/main/java/com/example/eloitteproject/MainActivity.java
@@ -10,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //jasmine is the best person in the world
+
     }
 }
