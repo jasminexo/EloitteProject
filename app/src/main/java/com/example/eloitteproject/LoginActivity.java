5c3e7229bfd9b903a13fef991770fb1d5980c549
jasminexo <56755376+jasminexo@users.noreply.github.com>
2022-03-08 17:49:33 +1100

    Finished LoginActivity

--- app/src/main/java/com/example/eloitteproject/LoginActivity.java
+++ app/src/main/java/com/example/eloitteproject/LoginActivity.java
@@ -1,14 +1,20 @@
 package com.example.eloitteproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
@@ -37,9 +43,59 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
     @Override
     public void onClick(View view) {
-
+        switch (view.getId()) {
+            case R.id.btnLogin:
+                loginUser();
+                break;
+        }
     }
+    //LOGIN FUNCTION: method for user login
+    //video reference: CodeWithMazn (2020), https://www.youtube.com/watch?v=Z-RE1QuUWPg
+    private void loginUser() {
+        String email = etEmail.getText().toString().trim();
+        String password = etPassword.getText().toString().trim();
 
+        //error msg if no email inputted
+        if (email.isEmpty()) {
+            etEmail.setError("Email is required!");
+            etEmail.requestFocus();
+            return;
+        }
+        //error msg if email does not contain .com or @
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            etEmail.setError("Please provide a valid email!");
+            etEmail.requestFocus();
+            return;
+        }
+        //error msg if no password inputted
+        if (password.isEmpty()) {
+            etPassword.setError("Password is required!");
+            etPassword.requestFocus();
+            return;
+        }
+
+        //make progressBar visible when user clicks login for user feedback
+        progressBar.setVisibility(View.VISIBLE);
+        //Firebase checks email and password
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                if (task.isSuccessful()) {
+                    //redirects to home activity
+                    startActivity(new Intent(LoginActivity.this, HomeActivity.class));
+                    //creates a toast to provide feedback if login successful or not
+                    Toast.makeText(getApplicationContext(), "Successful Login!", Toast.LENGTH_LONG).show();
+                    progressBar.setVisibility(View.GONE);
+
+                } else {
+                    Toast.makeText(getApplicationContext(), "Failed to login! Please check your credentials!",
+                            Toast.LENGTH_LONG).show();
+                    progressBar.setVisibility(View.GONE);
+                }
+            }
+        });
+    }
 
     //redirects to the RegisterActivity
     public void goToRegister(View view) {
--- app/src/main/java/com/example/eloitteproject/RegisterActivity.java
+++ app/src/main/java/com/example/eloitteproject/RegisterActivity.java
@@ -1,14 +1,147 @@
 package com.example.eloitteproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
-public class RegisterActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button register;
+    TextInputEditText etName, etEmail, etUsername, etPassword;
+    ProgressBar progressBar;
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        register = findViewById(R.id.btnLogin);
+        register.setOnClickListener(this);
+
+        etName = findViewById(R.id.etName);
+        etEmail = findViewById(R.id.etEmail);
+        etUsername = findViewById(R.id.etEmailLogin);
+        etPassword = findViewById(R.id.etPassword);
+
+        progressBar = findViewById(R.id.progressBarRegister);
+
     }
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btnLogin:
+                registerUser();
+                break;
+        }
+
+    }
+    //REGISTER FUNCTION: method for user registration
+    private void registerUser() {
+        String name = etName.getText().toString().trim();
+        String email = etEmail.getText().toString().trim();
+        String username = etUsername.getText().toString().trim();
+        String password = etPassword.getText().toString().trim();
+
+        //error msg if no name is inputted
+        if(name.isEmpty()) {
+            etName.setError("Full name is required!");
+            etName.requestFocus();
+            return;
+        }
+
+        //error msg if no email inputted
+        if(email.isEmpty()) {
+            etEmail.setError("Email is required!");
+            etEmail.requestFocus();
+            return;
+        }
+        //error msg if email does not contain .com or @
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            etEmail.setError("Please provide a valid email!");
+            etEmail.requestFocus();
+            return;
+        }
+        //error msg if no username inputted
+        if(username.isEmpty()) {
+            etUsername.setError("Username is required!");
+            etUsername.requestFocus();
+            return;
+        }
+        //error msg if no password inputted
+        if(password.isEmpty()) {
+            etPassword.setError("Password is required!");
+            etPassword.requestFocus();
+            return;
+        }
+        //error msg if password is less than 6 characters
+        if(password.length() < 6) {
+            etPassword.setError("Minimum password length should be 6 characters!");
+            etPassword.requestFocus();
+            return;
+        }
+
+        //make progressBar visible when user clicks login for user feedback
+        progressBar.setVisibility(View.VISIBLE);
+        //Firebase creates user with email and password
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if(task.isSuccessful()) {
+                            User user = new User (name, username, email);
+
+                            FirebaseDatabase.getInstance().getReference("Users")
+                                    .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                                    .setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+
+                                    if(task.isSuccessful()) {
+                                        //creates a toast to provide feedback if login successful or not
+                                        Toast.makeText(getApplicationContext(), "User has been registered successfully!", Toast.LENGTH_LONG).show();
+                                        //Redirects to the LoginActivity
+                                        startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+                                        progressBar.setVisibility(View.GONE);
+
+                                    } else {
+                                        Toast.makeText(getApplicationContext(), "Failed to register! Try Again!", Toast.LENGTH_LONG).show();
+                                        progressBar.setVisibility(View.GONE);
+                                    }
+                                }
+                            });
+
+                        } else {
+                            Toast.makeText(getApplicationContext(), "Failed to register!", Toast.LENGTH_LONG).show();
+                            progressBar.setVisibility(View.GONE);
+                        }
+                    }
+                });
+    }
+
+    //redirects to the LoginActivity
+    public void goToLogin(View view) {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+        //Removes animation when switching to loginActivity
+        overridePendingTransition(0,0);
+    }
+
 }
