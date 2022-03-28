e7434d251e5b0b205db5e89942f573f1f835f009
m9039 <61225254+m9039@users.noreply.github.com>
2022-03-28 15:55:40 +1100

    quiz home xml

---
+++ app/src/main/java/com/example/eloitteproject/StudentQuizActivity.java
@@ -0,0 +1,26 @@
+package com.example.eloitteproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class StudentQuizActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_quiz);
+    }
+
+    public void goToStudentHomeActivity(View view){
+        Intent intent = new Intent (this, StudentHomeActivity.class);
+        startActivity(intent);
+    }
+
+    public void goToStudentProfileActivity(View view){
+        Intent intent = new Intent (this, StudentProfileActivity.class);
+        startActivity(intent);
+    }
+}
