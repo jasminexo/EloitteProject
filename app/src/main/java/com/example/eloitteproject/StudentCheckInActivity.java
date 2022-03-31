0e4fbd18a8c3fc94a7d3829df67aa5fe10a83ccd
m9039 <61225254+m9039@users.noreply.github.com>
2022-04-01 09:58:51 +1100

    functional next button - need to stop checkin after 5qs

--- app/src/main/java/com/example/eloitteproject/StudentCheckInActivity.java
+++ app/src/main/java/com/example/eloitteproject/StudentCheckInActivity.java
@@ -19,7 +19,10 @@ public class StudentCheckInActivity extends AppCompatActivity {
     private ImageView ivImage;
     private Button btnNext;
     private SeekBar seekBar;
+    private String qID;
+    CheckInQuestionDatabase qDB;
     int currentScore = 0, currentQuestionPosition = 1, seekBarProgress = 3, currentArrayPosition = 0;
+    int q1Score, q2Score, q3Score, q4Score, q5Score;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,39 +35,15 @@ public class StudentCheckInActivity extends AppCompatActivity {
         btnNext = findViewById(R.id.btnNext);
         seekBar = findViewById(R.id.seekBar);
 
-        seekBarChanged();
-        displayCheckInQuestion();
-
-    }
-
-    private void displayCheckInQuestion() {
-
         //Database
-        CheckInQuestionDatabase qDB = Room.databaseBuilder(getApplicationContext(), CheckInQuestionDatabase.class ,
+        qDB = Room.databaseBuilder(getApplicationContext(), CheckInQuestionDatabase.class ,
                 "check-in-question-database")
                 .build();
 
-        //Assign a question from the bank
-        Executors.newSingleThreadExecutor().execute(new Runnable() {
-            @Override
-            public void run() {
-                for(CheckInQuestion q : CheckInQuestion.getCheckInQuestionList()){
-                        qDB.checkInQuestionDao().insert(q);
-                }
-                String qID = "q"+ currentQuestionPosition;
-                CheckInQuestion desiredQuestion = qDB.checkInQuestionDao().getCheckInQuestion(qID);
-                String checkInQuestion = desiredQuestion.getQuestion();
-                Integer checkInImage = desiredQuestion.getImage();
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        tvQuestionNumber.setText(currentQuestionPosition+"/5");
-                        tvQuestion.setText(checkInQuestion);
-                        ivImage.setImageResource(checkInImage);
-                    }
-                });
-            }
-        });
+        seekBarChanged();
+        displayCheckInQuestion();
+        nextButtonClicked();
+
     }
 
     //when user changes the seekbar
@@ -83,6 +62,73 @@ public class StudentCheckInActivity extends AppCompatActivity {
 
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
+    }
+
+    private void displayCheckInQuestion() {
+        //Assign a question from the bank
+        Executors.newSingleThreadExecutor().execute(new Runnable() {
+            @Override
+            public void run() {
+                for(CheckInQuestion q : CheckInQuestion.getCheckInQuestionList()){
+                        qDB.checkInQuestionDao().insert(q);
+                }
+                qID = "q"+ currentQuestionPosition;
+                CheckInQuestion desiredQuestion = qDB.checkInQuestionDao().getCheckInQuestion(qID);
+                String checkInQuestion = desiredQuestion.getQuestion();
+                Integer checkInImage = desiredQuestion.getImage();
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        tvQuestionNumber.setText(currentQuestionPosition+"/5");
+                        tvQuestion.setText(checkInQuestion);
+                        ivImage.setImageResource(checkInImage);
+                    }
+                });
+            }
+        });
+    }
+
+    private void nextButtonClicked() {
+        //saving the score for each question - need to find a way to upload into database with date and time
+        if (qID == "q1"){
+            q1Score = seekBarProgress;
+        } else if (qID == "q2"){
+            q2Score = seekBarProgress;
+        } else if (qID == "q3"){
+            q3Score = seekBarProgress;
+        } else if (qID == "q4"){
+            q4Score = seekBarProgress;
+        } else if (qID == "q5"){
+            q5Score = seekBarProgress;
+        }
+
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                currentQuestionPosition++;
+                //Assign a question from the bank
+                Executors.newSingleThreadExecutor().execute(new Runnable() {
+                    @Override
+                    public void run() {
+                        for(CheckInQuestion q : CheckInQuestion.getCheckInQuestionList()){
+                            qDB.checkInQuestionDao().insert(q);
+                        }
+                        qID = "q"+ currentQuestionPosition;
+                        CheckInQuestion desiredQuestion = qDB.checkInQuestionDao().getCheckInQuestion(qID);
+                        String checkInQuestion = desiredQuestion.getQuestion();
+                        Integer checkInImage = desiredQuestion.getImage();
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                tvQuestionNumber.setText(currentQuestionPosition+"/5");
+                                tvQuestion.setText(checkInQuestion);
+                                ivImage.setImageResource(checkInImage);
+                            }
+                        });
+                    }
+                });
             }
         });
     }
