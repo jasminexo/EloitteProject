995d677e9254addddd92b7d4c8f2a32afde55d63
jasminexo <56755376+jasminexo@users.noreply.github.com>
2022-04-05 01:50:06 +1000

    Goals Setting Half Complete ;_;

--- app/src/main/java/com/example/eloitteproject/StudentHomeActivity.java
+++ app/src/main/java/com/example/eloitteproject/StudentHomeActivity.java
@@ -8,6 +8,9 @@ import android.view.View;
 import android.widget.Button;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class StudentHomeActivity extends AppCompatActivity {
 
@@ -24,6 +27,7 @@ public class StudentHomeActivity extends AppCompatActivity {
                 finish();
             }
         });
+
     }
 
     public void goToCheckInActivity(View view){
@@ -51,9 +55,14 @@ public class StudentHomeActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    public void goToStudentProfileActivity(View view){
-        Intent intent = new Intent (this, StudentProfileActivity.class);
-        startActivity(intent);
+//    public void goToStudentProfileActivity(View view){
+//        Intent intent = new Intent (this, StudentProfileActivity.class);
+//        startActivity(intent);
+
+        public void goToStudentProfileActivity(View view){
+            Intent intent = new Intent (this, StudentGoalsActivity.class);
+            startActivity(intent);
     }
     //testing
+
 }
