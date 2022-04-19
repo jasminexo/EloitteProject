ee9754606ab5cdeba0a80d5ece74ab84c8bc1b6b
m9039 <61225254+m9039@users.noreply.github.com>
2022-04-19 17:51:30 +1000

    teacher home

---
+++ app/src/main/java/com/example/eloitteproject/TeacherAppointmentActivity.java
@@ -0,0 +1,14 @@
+package com.example.eloitteproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TeacherAppointmentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_teacher_appointment);
+    }
+}
